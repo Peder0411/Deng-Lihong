521cfffa38a265f5be9886b32f3011370c626a3e
Peter0411 <262901609@qq.com>
2024-10-20 11:00:38 +0800

    fist commit

---
+++ demo/src/main/java/com/practice/entity/User.java
@@ -0,0 +1,40 @@
+package com.practice.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author liruirui
+ * @since 2024-10-16
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="User对象", description="")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+    private String phone;
+    private String address;
+    private String post;
+    private String status;
+}
